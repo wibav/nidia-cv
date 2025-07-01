b07eb6b76a6414f926eb03c23bb0e1c0584f4015
Cristian Da Silva <cdasilva@nexo-it.cl>
2025-07-01 19:27:45 +0100

    Actualizar configuración de Firebase y eliminar código no utilizado

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        unoptimized: true
+    },
+    distDir: 'out'
+}
+
+export default nextConfig
